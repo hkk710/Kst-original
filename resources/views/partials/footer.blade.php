42541d0a3e946da99a3aca58a8d1471e2e6961de
hkk710 <hkk710@gmail.com>
2018-10-13 22:52:59 +0530

    welcome page updated

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="w-100 bg-kst text-light text-center py-2 mt-3 rounded-top">
+	<div class="row">
+		<div class="col-md-6">
+			Copyright &copy; {{ date('Y') }} | All rights reserved | Developed by HK Web Developer
+		</div>
+		<div class="col-md-6">
+				Number of visitors:
+				<a href="http://www.webfreecounter.com/" target="_blank"><img src="http://www.cutercounter.com/hit.php?id=24250&nd=6&style=6" border="0" alt="hit counter"></a>
+		</div>
+	</div>
+</footer>
